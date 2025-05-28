c33f09e9ed060553434527c90331db44a6880033
aptgetmams <louis.mamelle@gmail.com>
2025-05-28 13:11:50 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-# Mise à jour du système
-sudo apt-get update && sudo apt-get upgrade -y
+### Mise à jour du système
+sudo apt update
+sudo apt full-upgrade -y
 
-# Dépendances logicielles
-sudo apt-get install -y \
+### Installation des dépendances C/C++
+sudo apt install -y \
     cmake \
     libusb-1.0-0-dev \
+    libxmu-dev \
+    libxi-dev \
     freeglut3-dev \
-    build-essential \
+    libglfw3-dev \
+    libturbojpeg0-dev \
+    libudev-dev \
     python3-dev \
     python3-pip \
-    python3-numpy \
-    libopenblas-dev
+    python3-numpy
 
-# Installation de libfreenect
-git clone https://github.com/OpenKinect/libfreenect
+### Compilation de libfreenect
 cd libfreenect
-mkdir build
+mkdir -p build
 cd build
-cmake .. -DBUILD_PYTHON3=ON
+cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_FAKENECT=OFF -DBUILD_C_SYNC=ON -DBUILD_PYTHON3=ON
 make
 sudo make install
-cd ../bindings/python
+cd ../wrappers/python
 sudo python3 setup.py install
+cd ../../../
 
-# Règles udev
-sudo cp ../../platform/linux/udev/90-kinect.rules /etc/udev/rules.d/
+### Règles udev
+sudo cp libfreenect/platform/linux/udev/90-kinect.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 
-# Installation des dépendances Python
+### Installation des dépendances Python
 sudo pip3 install -r requirements.txt
 
-echo "Installation terminée! Redémarrez le système."
+echo "Installation réussie! Redémarrez le système."
