57de2c2534ecfd24b38f8dbe1777be868bfd2e18
aptgetmams <louis.mamelle@gmail.com>
2025-06-03 09:09:24 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 sudo apt update && sudo apt full-upgrade -y 
 
+sudo apt install git cmake build-essential libusb-1.0-0-dev libturbojpeg0-dev
+
+#sudo apt install libusb-1.0-0-dev python3-opencv python3-numpy 
+#python3-pyopengl
+
 sudo apt update && sudo apt install -y \
     libusb-1.0-0-dev \
     python3-opencv \
     python3-numpy \
-    python3-pyopengl
-
-sudo apt install git cmake build-essential libusb-1.0-0-dev libturbojpeg0-dev
+#    python3-pyopengl
 
 sudo apt install -y libusb-1.0-0-dev libgl1-mesa-dev python3-opencv
 
-sudo apt install -y \ 
-    libopencv-dev \ 
-    python3-dev \ 
-    python3-pybind11 \ 
-    cmake \ 
-    freeglut3-dev \ 
-    libgl1-mesa-dev \ 
-    ninja-build
+sudo apt install libopencv-dev python3-dev python3-pybind11 cmake freeglut3-dev libgl1-mesa-dev ninja-build
+
+#sudo apt install -y \ 
+#    libopencv-dev \ 
+#    python3-dev \ 
+#    python3-pybind11 \ 
+#    cmake \ 
+#    freeglut3-dev \ 
+#    libgl1-mesa-dev \ 
+#    ninja-build
 
 # Compilation optimisée de libfreenect
 git clone https://github.com/OpenKinect/libfreenect
@@ -28,5 +33,6 @@ cmake .. -DCMAKE_C_FLAGS="-march=armv7-a -mtune=cortex-a53 -mfpu=neon-fp-armv8"
 make -j$(nproc) && sudo make install
 
 # Configuration udev
-sudo cp ../99-kinect.rules /etc/udev/rules.d/
-sudo udevadm control --reload
+sudo cp /home/pi/KinectGenerativeArt/libfreenect/platform/linux/udev/51-kinect.rules /etc/udev/rules.d/
+sudo cp /home/pi/KinectGenerativeArt/99-kinect.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
